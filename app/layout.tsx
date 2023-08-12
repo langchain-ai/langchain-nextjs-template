7de43a34819a460d04d14f01a46a44bf6b17ec94
jacoblee93 <jacoblee93@gmail.com>
2023-08-12 12:42:11 -0700

    Adds OG tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
       <head>
         <title>LangChain + Next.js Template</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
+        <meta name="description" content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"/>
+        <meta property="og:title" content="LangChain + Next.js Template" />
+        <meta property="og:description" content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!" />
+        <meta property="og:image" content="/images/og-image.png" />
       </head>
       <body className={publicSans.className}>
         <div className="flex flex-col p-4 md:p-12 h-[100vh]">
