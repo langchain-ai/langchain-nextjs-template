677cd5bc3014a550d0dcccc5fb6e3052a2546466
jacoblee93 <jacoblee93@gmail.com>
2023-08-12 12:47:31 -0700

    Adds Twitter meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
         <meta property="og:title" content="LangChain + Next.js Template" />
         <meta property="og:description" content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!" />
         <meta property="og:image" content="/images/og-image.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="LangChain + Next.js Template" />
+        <meta name="twitter:description" content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!" />
+        <meta name="twitter:image" content="/images/og-image.png" />
       </head>
       <body className={publicSans.className}>
         <div className="flex flex-col p-4 md:p-12 h-[100vh]">
