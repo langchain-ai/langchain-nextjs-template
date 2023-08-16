8ac567fb8fd6e60d06220512f21e6b9faa437de5
jacoblee93 <jacoblee93@gmail.com>
2023-08-16 15:41:06 -0700

    Revert

--- app/api/chat/agents/route.ts
+++ app/api/chat/agents/route.ts
@@ -63,6 +63,7 @@ export async function POST(req: NextRequest) {
       memory: new BufferMemory({
         memoryKey: "chat_history",
         chatHistory: new ChatMessageHistory(previousMessages),
+        returnMessages: true,
         outputKey: "output",
       }),
       agentArgs: {
