4f1446dbd70f60a21d950cb2a3edd89674d8c231
jacoblee93 <jacoblee93@gmail.com>
2023-08-23 19:20:25 -0700

    Fix alignment

--- components/ChatMessageBubble.tsx
+++ components/ChatMessageBubble.tsx
@@ -4,7 +4,7 @@ export function ChatMessageBubble(props: { message: Message, aiEmoji?: string })
   const colorClassName =
     props.message.role === "user" ? "bg-sky-600" : "bg-slate-50 text-black";
   const alignmentClassName =
-    props.message.role === "user" ? "mr-auto" : "ml-auto";
+    props.message.role === "user" ? "ml-auto" : "mr-auto";
   const prefix = props.message.role === "user" ? "🧑" : props.aiEmoji;
   return (
     <div
