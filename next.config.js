52bdc0c6d1c3cf353ac2fe95ace86facaf79dcc2
jacoblee93 <jacoblee93@gmail.com>
2023-09-09 09:36:59 -0700

    Bump LangChain version, add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
