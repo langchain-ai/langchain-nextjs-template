5620e39eeb81134691b771284632ba9c8db76811
jacoblee93 <jacoblee93@gmail.com>
2023-10-04 01:04:14 -0700

    Lower temperature

--- app/api/chat/retrieval/route.ts
+++ app/api/chat/retrieval/route.ts
@@ -80,6 +80,7 @@ export async function POST(req: NextRequest) {
 
     const model = new ChatOpenAI({
       modelName: "gpt-3.5-turbo",
+      temperature: 0.2
     });
 
     const client = createClient(
